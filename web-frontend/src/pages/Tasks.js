d240355079b96ad7910a937b07a5f7b466acbd51
Marcio <alexandrino.marcio@gmail.com>
2019-10-09 16:38:45 -0300

    adding tasks page

--- web-frontend/src/pages/Tasks.js
+++ web-frontend/src/pages/Tasks.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../services/api';
+import './Tasks.css';
 
-export default function pages() {
+export default function Tasks({ location }) {
+  const [tasks, setTasks] = useState([]);
+  const token = location.state.token;
+
+  useEffect(() => {
+    async function loadTasks() {
+      try {
+        const response = await api.get('/tasks', {
+          headers: { "Authorization": token }
+        });
+        if (response.status === 200) {
+          console.log(response.data);
+          setTasks(response.data);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    loadTasks();
+  }, [token]);
+
   return (
-    <div >
-        <p>Tasks</p>
+    <div className="content">
+      {tasks.map((task) => (
+        <div className="task" key={task._id}>
+          <p>{task.description}</p>
+          <input
+            type="checkbox"
+            name="completed"
+            id="completed"
+            checked={task.completed}
+            onChange={()=>{}}
+          />
+        </div>
+      ))}
     </div>
   );
 }
